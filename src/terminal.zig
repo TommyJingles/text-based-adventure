77e638e3dbf3c4817eda6e3db0122e3823f4985f
Tommy Jingles <tommy.jingles@yahoo.com>
2023-12-03 09:57:21 -0600

    add basic input history and ansi support

--- src/terminal.zig
+++ src/terminal.zig
@@ -7,26 +7,67 @@ pub extern "kernel32" fn SetConsoleOutputCP(
 
 pub const Terminal = struct {
     alloc: std.mem.Allocator,
-    buffer: std.ArrayList(u8),
+    in_buf: std.ArrayList(u8),
     writer: std.fs.File.Writer,
+    out_buf: std.ArrayList(u8),
     reader: std.fs.File.Reader,
+    input: std.ArrayList(std.ArrayList(u8)),
 
     pub fn init(alloc: std.mem.Allocator) Terminal {
+        // configs output to accept unicode
         _ = SetConsoleOutputCP(65001);
-        return Terminal{ .alloc = alloc, .buffer = std.ArrayList(u8).init(alloc), .writer = std.io.getStdOut().writer(), .reader = std.io.getStdIn().reader() };
+
+        return Terminal{
+            .alloc = alloc,
+            .in_buf = std.ArrayList(u8).init(alloc),
+            .writer = std.io.getStdOut().writer(),
+            .out_buf = std.ArrayList(u8).init(alloc),
+            .reader = std.io.getStdIn().reader(),
+            .input = std.ArrayList(std.ArrayList(u8)).init(alloc),
+        };
     }
 
     pub fn deinit(self: *Terminal) void {
-        self.buffer.deinit();
+        self.in_buf.deinit();
+        for (self.input.items) |str| {
+            str.deinit();
+        }
+        self.input.deinit();
     }
 
-    pub fn write(self: *Terminal, comptime fmt: []const u8, args: anytype) !void {
+    pub fn write(self: *Terminal, str: []const u8) !void {
+        try self.out_buf.writer().writeAll(str);
+    }
+
+    pub fn write_fmt(self: *Terminal, comptime fmt: []const u8, args: anytype) !void {
+        try std.fmt.format(self.out_buf.writer(), fmt, args);
+    }
+
+    pub fn write_now(self: *Terminal, comptime fmt: []const u8, args: anytype) !void {
         try std.fmt.format(self.writer, fmt, args);
     }
 
+    pub fn flush(self: *Terminal) !void {
+        try self.writer.writeAll(self.out_buf.items);
+        try self.out_buf.resize(0);
+    }
+
     pub fn read(self: *Terminal) ![]const u8 {
-        try self.buffer.resize(0);
-        try self.reader.readUntilDelimiterArrayList(&self.buffer, '\n', 4096);
-        return std.mem.trimRight(u8, self.buffer.items, "\r");
+        try self.in_buf.resize(0);
+        try self.reader.readUntilDelimiterArrayList(&self.in_buf, '\n', 4096);
+        const slice = std.mem.trimRight(u8, self.in_buf.items, "\r");
+        var str = try std.ArrayList(u8).initCapacity(self.alloc, slice.len);
+        try str.appendSlice(slice);
+        try self.input.append(str);
+        return str.items;
+    }
+    pub fn dump_input(self: *Terminal) !void {
+        var i: usize = 0;
+        try self.write("\n\n------ INPUT HISTORY ------");
+        for (self.input.items) |str| {
+            try self.write_fmt("\n{}:   {s}", .{ i, str.items });
+            i += 1;
+        }
+        try self.flush();
     }
 };
